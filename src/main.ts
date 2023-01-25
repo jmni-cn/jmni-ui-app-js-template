95f19300a31a6ab826f20d7e4979ced6f95eed1f
DESKTOP-OLEAEOR\17110 <1711008052@qq.com>
2023-01-25 16:28:19 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import JmniUI from "jmni-ui-vite";
+import "jmni-ui-vite/style.css";
+
+createApp(App)
+    .use(JmniUI).mount('#app')
